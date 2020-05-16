bb2628673566a5c08b092b221aeb69ff3b841146
Opinder <opinder79@gmail.com>
2020-05-16 23:36:19 +0100

    May 16, 2020 - tuts

--- Basic-Python-Tutorials.py
+++ Basic-Python-Tutorials.py
@@ -1,5 +1,31 @@
-num1 = int(input("Enter First Number : "))
-num2 = int(input("Enter Second Number : "))
-res = num1 + num2
-print("The Sum Of " + str(num1) + " and " + str(num2) + " is equals to = " + str(res))
- 
+#Example 1
+# num1 = int(input("Enter First Number : "))
+# num2 = int(input("Enter Second Number : "))
+# res = num1 + num2
+# print("The Sum Of " + str(num1) + " and " + str(num2) + " is equals to = " + str(res))
+
+
+#Example2
+# hello = "Hello"
+# world = 'World'
+# hello_world = hello + ' ' + world
+# print(hello_world)      # Note: you should print "Hello World"
+
+
+#Example 3
+# phrase = """
+# It is a really long string
+# triple-quoted strings are used
+# to define multi-line strings
+# """
+# first_half = phrase[:int(len(phrase)/2)]
+# print(first_half)
+
+
+#Example 4
+# name = "John"
+# age = 30
+# print("Hello, PyCharm! My name is %s!" % name)     # Note: %s is inside the string, % is after the string
+# print("I'm %d years old" % age)
+
+#Example 5
