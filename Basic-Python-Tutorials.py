58be4124928117adffabf59d98ffd405cdd269f1
Opinder <opinder79@gmail.com>
2020-05-16 21:15:07 +0100

    Basic Tuts 1

---
+++ Basic-Python-Tutorials.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+res = num1 + num2
+print("The Sum Of " + str(num1) + " and " + str(num2) + " is equals to = " + str(res))
+ 
